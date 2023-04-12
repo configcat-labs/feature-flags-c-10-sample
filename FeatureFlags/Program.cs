30ce7d2e1993af6f68d14e727ab37c7ff1f59427
Rox H <roxana.halati@gmail.com>
2023-04-12 18:36:13 +0300

    C#10 Sample App

---
+++ FeatureFlags/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using ConfigCat;
+using ConfigCat.Client;
+
+//Item to be purchased
+public record Item
+{
+    public decimal Price { get; set; }
+
+    //can have name, id, description etc.
+}
+
+//Handles all the items
+public class Cart
+{
+    private readonly bool _discount;
+
+
+    public Cart(bool discount)
+    {
+        _discount = discount;
+    }
+
+    public decimal CalculateTotal(List<Item> purchases)
+    {
+
+        decimal totalAmount = 0;
+
+        foreach (var purchase in purchases)
+        {
+            totalAmount += purchase.Price;
+
+
+        }
+
+        if (_discount)
+        {
+            totalAmount *= 0.9m; // Apply 10% discount
+        }
+
+        return totalAmount;
+
+    }
+}
+
+
+public class Program
+{
+
+        static void Main()
+        {
+
+
+            User user = new User("1275") // Unique identifier is required.
+            {
+
+                Custom =
+                {
+                    { "Type", "Member"}, //Member or Non-Member
+                }
+            };
+
+            var client = ConfigCatClient.Get("YOUR-SDK-KEY");
+
+            var discountApplies = client.GetValue("discountApplies", false, user);
+
+            Console.WriteLine("discountApplies's value from ConfigCat: " + discountApplies);
+
+            Cart newCart = new Cart(discountApplies);
+
+            // Simulate multiple purchases
+            List<Item> purchases = new List<Item>
+            {
+                new Item { Price = 100 },
+                new Item { Price = 50},
+                new Item { Price = 50}
+            };
+
+        //Calculate and display the total amount to be paid
+        Console.WriteLine("Total Amount to Pay Today: " + newCart.CalculateTotal(purchases));
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# feature-flags-c-10-sample
+# Using Feature Flags in a C#10 Application
+
+This repository contains a sample app for using feature flags in a C#10 console application. The feature flags are handled using [ConfigCat](https://configcat.com). The app is a simple mock infrastructure for an online e-commerce website where members are given a 10% of their entire shopping cart.
+
+## Build & Run
+
+### Prerequisites
+
+- Visual Studio
+- C# and .NET knowledge
+
+### Running the app
+
+- Clone the repository
+- Replace the SDK key in Program.cs with your own key from the ConfigCat dashboard
+- Run the application for VS
+
+## Learn more
+
+- [C#10 Documentation](https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-10)
+- [ConfigCat .NET SDK Documentation](https://configcat.com/docs/sdk-reference/dotnet/)
+
+[**ConfigCat**](https://configcat.com) also supports many other frameworks and languages. Check out the full list of supported SDKs [here](https://configcat.com/docs/sdk-reference/overview/).
+
+You can also explore other code samples for various languages, frameworks, and topics here in the [ConfigCat labs](https://github.com/configcat-labs) on GitHub.
+
+Keep up with ConfigCat on [Twitter](https://twitter.com/configcat), [Facebook](https://www.facebook.com/configcat), [LinkedIn](https://www.linkedin.com/company/configcat/), and [GitHub](https://github.com/configcat).
+
+## Author
+
+[Roxana Halați](https://www.linkedin.com/in/roxanahalati/)
+
+## Contributions
+
+Contributions are welcome!
